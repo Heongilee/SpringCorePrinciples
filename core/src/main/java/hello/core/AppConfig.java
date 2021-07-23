92c4b39f9dfa26237b8859953da7576eca606bf8
Heongilee <gjsrl1@gmail.com>
2021-07-23 10:28:53 +0900

    스프링으로 전환하기 외 1

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,25 +9,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // memberRepository에 대한건 memoryMemberRepository를 쓸거야.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 현재 나의 애플리케이션에서 memberService는 memberServiceImpl을 쓸거야.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // discountPolicy에 대한건 정액할인정책(FixDiscountPolicy)를 쓸거야.
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
 //        return new FixDiscountPolicy();
     }
 
     // 현재 나의 애플리케이션에서 orderService는 orderServiceImpl을 쓸거야.
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
